a68eccd10b6871522eb6a7ae31f7bfe66c6220b7
LeviRomans <40896186+LeviRomans@users.noreply.github.com>
2022-03-04 20:17:30 -0500

    Add Components folder for React Components Organization

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 // "./" denotes the path relative to this current file
-import App from "./App";
+import App from "./Components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
